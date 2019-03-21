5c3dd1e7d428545d0005bb1485df4b6608558f6a
Eulogio Gutierrez <eulogio.gutierrez@gridsmartercities.com>
2019-03-21 16:23:56 +0000

    added get_headers

--- aws_v4signer/aws_v4signer.py
+++ aws_v4signer/aws_v4signer.py
@@ -2,13 +2,43 @@ import datetime
 import hashlib
 import hmac
 
-ENCODING = 'utf-8'
 VERSION = 'AWS4'
+ALGORITHM = 'AWS4-HMAC-SHA256'
+SIGNED_HEADERS = 'host;x-amz-date'
 AWS4_REQUEST = 'aws4_request'
+ENCODING = 'utf-8'
+AMZ_DATE_FORMAT = '%Y%m%dT%H%M%SZ'
 DATE_STAMP_FORMAT = '%Y%m%d'
 
 
 class AWSV4Signer:
+
+    def __init__(self, aws_key, aws_secret, aws_region, aws_service, aws_host):  # pylint:disable=too-many-arguments
+        self._key = aws_key
+        self._secret = aws_secret
+        self._region = aws_region
+        self._service = aws_service
+        self._host = aws_host
+
+    @property
+    def key(self):
+        return self._key
+
+    @property
+    def secret(self):
+        return self._secret
+
+    @property
+    def region(self):
+        return self._region
+
+    @property
+    def service(self):
+        return self._service
+
+    @property
+    def host(self):
+        return self._host
 
     @staticmethod
     def sign(key, message):
@@ -22,12 +52,45 @@ class AWSV4Signer:
         k_signing = AWSV4Signer.sign(k_service, AWS4_REQUEST)
         return k_signing
 
+    def get_headers(self, uri, method, querystring, body):
+        now = datetime.datetime.utcnow()
+        amz_date = now.strftime(AMZ_DATE_FORMAT)
+        date_stamp = now.strftime(DATE_STAMP_FORMAT)
 
-if __name__ == '__main__':
+        canonical_request = method + '\n' \
+            + uri + '\n' \
+            + querystring + '\n' \
+            + 'host:' + self._host + '\n' \
+            + 'x-amz-date:' + amz_date + '\n' \
+            + '\n' \
+            + SIGNED_HEADERS + '\n' \
+            + hashlib.sha256(body.encode(ENCODING)).hexdigest()
 
-    print(AWSV4Signer.sign(''.encode(ENCODING), ''))
+        credential_scope = date_stamp + '/' + self._region + '/' + self._service + '/' + AWS4_REQUEST
 
-    print(AWSV4Signer.get_signature_key('YOUR_AWS_SECRET_KEY',
-                                        datetime.datetime.utcnow().strftime(DATE_STAMP_FORMAT),
-                                        'YOUR_AWS_REGION',
-                                        'YOUR_AWS_SERVICE'))
+        string_to_sign = ALGORITHM + '\n' \
+            + amz_date + '\n' \
+            + credential_scope + '\n' \
+            + hashlib.sha256(canonical_request.encode(ENCODING)).hexdigest()
+
+        signing_key = AWSV4Signer.get_signature_key(self._secret, date_stamp, self._region, self._service)
+
+        authorization_header = ALGORITHM + ' ' \
+            + 'Credential=' + self._key + '/' + credential_scope + ', ' \
+            + 'SignedHeaders=' + SIGNED_HEADERS + ', ' \
+            + 'Signature=' + hmac.new(signing_key, string_to_sign.encode(ENCODING), hashlib.sha256).hexdigest()
+
+        return {
+            'x-amz-date': amz_date,
+            'Authorization': authorization_header
+        }
+
+
+# if __name__ == '__main__':
+#
+#     print(AWSV4Signer.sign(''.encode(ENCODING), ''))
+#
+#     print(AWSV4Signer.get_signature_key('YOUR_AWS_KEY_ID',
+#                                         datetime.datetime.utcnow().strftime(DATE_STAMP_FORMAT),
+#                                         'YOUR_AWS_REGION',
+#                                         'YOUR_AWS_SERVICE'))
--- tests/test_aws_v4signer.py
+++ tests/test_aws_v4signer.py
@@ -1,13 +1,25 @@
 import datetime
+import json
 import unittest
 from aws_v4signer.aws_v4signer import AWSV4Signer, ENCODING, DATE_STAMP_FORMAT
 
 
 class AWSV4SignerTests(unittest.TestCase):
 
-    def test_class_exists(self):
-        v4_signer = AWSV4Signer
-        self.assertTrue(v4_signer.__name__ == 'AWSV4Signer')
+    def test_can_create_signer_with_parameters(self):
+        key = 'YOUR_AWS_KEY_ID'
+        secret = 'YOUR_AWS_KEY_SECRET'
+        region = 'YOUR_AWS_REGION'
+        service = 'YOUR_AWS_SERVICE'
+        host = 'YOUR_AWS_HOST'
+
+        v4_signer = AWSV4Signer(key, secret, region, service, host)
+
+        self.assertEqual(v4_signer.key, key)
+        self.assertEqual(v4_signer.secret, secret)
+        self.assertEqual(v4_signer.region, region)
+        self.assertEqual(v4_signer.service, service)
+        self.assertEqual(v4_signer.host, host)
 
     def test_can_sign_message_with_a_key(self):
         key = 'test'
@@ -19,12 +31,31 @@ class AWSV4SignerTests(unittest.TestCase):
                                          b'\t\xac\x0b\x12\xb4f\xc024')
 
     def test_can_return_signature_key(self):
-        key = 'YOUR_AWS_SECRET_KEY'
+        key = 'YOUR_AWS_KEY_ID'
         date = datetime.datetime.utcnow().strftime(DATE_STAMP_FORMAT)
         region = 'YOUR_AWS_REGION'
         service = 'YOUR_AWS_SERVICE'
 
         signature_key = AWSV4Signer.get_signature_key(key, date, region, service)
 
-        self.assertEqual(signature_key, b'k\x92t\xd3\xea>\x9b\xf0\x8f\xe9\xc7:\x0f\xa2\xec\xed!\x91\x19\xf4\x05\xaa'
-                                        b'\xde\xc1H\x14.\xd1\x02\xf9h\x1b')
+        self.assertEqual(signature_key, b'\x1f\x1e\x04\xc7\xb17\x93N\xfa\x84q\x95o\xe9\xa4u\xffDk\xeb\x92^\x03\x9d%'
+                                        b'\x8e\x95\x06GO\x1c\x1e')
+
+    def test_can_return_headers(self):
+        key = 'YOUR_AWS_KEY_ID'
+        secret = 'YOUR_AWS_KEY_SECRET'
+        region = 'YOUR_AWS_REGION'
+        service = 'YOUR_AWS_SERVICE'
+        host = 'YOUR_AWS_HOST'
+
+        v4_signer = AWSV4Signer(key, secret, region, service, host)
+
+        uri = ''
+        method = ''
+        querystring = ''
+        body = json.dumps({})
+
+        headers = v4_signer.get_headers(uri, method, querystring, body)
+
+        self.assertTrue(headers['Authorization'])
+        self.assertTrue(headers['x-amz-date'])
