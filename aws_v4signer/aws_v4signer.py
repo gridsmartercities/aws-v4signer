c2f09afc52296b67d56a1d4798ec545039457f7b
Eulogio Gutierrez <eulogio.gutierrez@gridsmartercities.com>
2019-03-21 15:01:46 +0000

    added method to get signature key

--- aws_v4signer/aws_v4signer.py
+++ aws_v4signer/aws_v4signer.py
@@ -1,16 +1,33 @@
+import datetime
 import hashlib
 import hmac
 
 ENCODING = 'utf-8'
+VERSION = 'AWS4'
+AWS4_REQUEST = 'aws4_request'
+DATE_STAMP_FORMAT = '%Y%m%d'
 
 
-class AWSV4Signer:  # pylint:disable=too-few-public-methods
+class AWSV4Signer:
 
     @staticmethod
     def sign(key, message):
         return hmac.new(key, message.encode(ENCODING), hashlib.sha256).digest()
 
+    @staticmethod
+    def get_signature_key(key, date, region, service):
+        k_date = AWSV4Signer.sign((VERSION + key).encode(ENCODING), date)
+        k_region = AWSV4Signer.sign(k_date, region)
+        k_service = AWSV4Signer.sign(k_region, service)
+        k_signing = AWSV4Signer.sign(k_service, AWS4_REQUEST)
+        return k_signing
 
-# if __name__ == '__main__':
-#
-#     print(AWSV4Signer.sign(''.encode(ENCODING), ''))
+
+if __name__ == '__main__':
+
+    print(AWSV4Signer.sign(''.encode(ENCODING), ''))
+
+    print(AWSV4Signer.get_signature_key('YOUR_AWS_SECRET_KEY',
+                                        datetime.datetime.utcnow().strftime(DATE_STAMP_FORMAT),
+                                        'YOUR_AWS_REGION',
+                                        'YOUR_AWS_SERVICE'))
--- tests/test_aws_v4signer.py
+++ tests/test_aws_v4signer.py
@@ -1,5 +1,6 @@
+import datetime
 import unittest
-from aws_v4signer.aws_v4signer import AWSV4Signer, ENCODING
+from aws_v4signer.aws_v4signer import AWSV4Signer, ENCODING, DATE_STAMP_FORMAT
 
 
 class AWSV4SignerTests(unittest.TestCase):
@@ -16,3 +17,14 @@ class AWSV4SignerTests(unittest.TestCase):
 
         self.assertEqual(signed_message, b'nv\xe21\x0b\xcd\n\xed{\xa4\xef\x80\x007\x9e\xae\x0f\xe0\x8b\x05*\x90\xdd'
                                          b'\t\xac\x0b\x12\xb4f\xc024')
+
+    def test_can_return_signature_key(self):
+        key = 'YOUR_AWS_SECRET_KEY'
+        date = datetime.datetime.utcnow().strftime(DATE_STAMP_FORMAT)
+        region = 'YOUR_AWS_REGION'
+        service = 'YOUR_AWS_SERVICE'
+
+        signature_key = AWSV4Signer.get_signature_key(key, date, region, service)
+
+        self.assertEqual(signature_key, b'k\x92t\xd3\xea>\x9b\xf0\x8f\xe9\xc7:\x0f\xa2\xec\xed!\x91\x19\xf4\x05\xaa'
+                                        b'\xde\xc1H\x14.\xd1\x02\xf9h\x1b')
