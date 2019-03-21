9f20ef1637a87cdb4d0f698739a81f248f8ad9e9
Eulogio Gutierrez <eulogio.gutierrez@gridsmartercities.com>
2019-03-21 14:55:26 +0000

    added sign method

--- aws_v4signer/aws_v4signer.py
+++ aws_v4signer/aws_v4signer.py
@@ -1,3 +1,16 @@
+import hashlib
+import hmac
+
+ENCODING = 'utf-8'
+
 
 class AWSV4Signer:  # pylint:disable=too-few-public-methods
-    pass
+
+    @staticmethod
+    def sign(key, message):
+        return hmac.new(key, message.encode(ENCODING), hashlib.sha256).digest()
+
+
+# if __name__ == '__main__':
+#
+#     print(AWSV4Signer.sign(''.encode(ENCODING), ''))
--- tests/test_aws_v4signer.py
+++ tests/test_aws_v4signer.py
@@ -1,5 +1,5 @@
 import unittest
-from aws_v4signer.aws_v4signer import AWSV4Signer
+from aws_v4signer.aws_v4signer import AWSV4Signer, ENCODING
 
 
 class AWSV4SignerTests(unittest.TestCase):
@@ -7,3 +7,12 @@ class AWSV4SignerTests(unittest.TestCase):
     def test_class_exists(self):
         v4_signer = AWSV4Signer
         self.assertTrue(v4_signer.__name__ == 'AWSV4Signer')
+
+    def test_can_sign_message_with_a_key(self):
+        key = 'test'
+        message = 'test message'
+
+        signed_message = AWSV4Signer.sign(key.encode(ENCODING), message)
+
+        self.assertEqual(signed_message, b'nv\xe21\x0b\xcd\n\xed{\xa4\xef\x80\x007\x9e\xae\x0f\xe0\x8b\x05*\x90\xdd'
+                                         b'\t\xac\x0b\x12\xb4f\xc024')
